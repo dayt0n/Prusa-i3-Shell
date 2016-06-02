5408a9cd57ff42319f9e612beaf4c2da1e9c53a8
Dayt0n <daytonhasty@gmail.com>
2016-06-02 15:37:28 -0500

    Added functions for SD usage, a shell, rebooting, and assigning coordinates

--- main.c
+++ main.c
@@ -12,26 +12,90 @@
 #include <termios.h>
 #include <sys/ioctl.h>
 #include <fcntl.h>
+#include <ctype.h>
 #include "serial_functions.h"
 
+int buf_max = 256;
+int timeout = 5000;
+
+void usage(char* argv) {
+	printf("usage: %s [OPTIONS] [/dev/cu.*]\n",argv);
+	printf("Options:\n\n-a [X/Y/Z] [0-150]\tassign the coordinate of an axis\n");
+	printf("-l\t\t\tlist all items on SD Card (if present)\n");
+	printf("-r\t\t\trestart printer\n");
+	printf("-s\t\t\topen a serial shell\n");
+}
+
+// for creating commands
+char* addVars(char *s1, char *s2)
+{
+    char *result = malloc(strlen(s1)+strlen(s2)+1);
+    strcpy(result, s1);
+    strcat(result, s2);
+    return result;
+}
+
+/* this doesn't move the axis at all, it just tells the printer new coordinates 
+ and you decide the stopping points */
+int assignCoordinate(int fd, char* buf, char* plane, char* coordinate) {
+	char* cmdDescript = "G92 ";
+	char* cmdOne = addVars(cmdDescript,plane);
+	char* cmdTwo = addVars(cmdOne," ");
+	char* cmdThree = addVars(cmdTwo,coordinate);
+	char* cmd = addVars(cmdThree,"\n");
+	int n = serial_write(fd,cmd);
+	if(n != 0)
+		return -1;
+	serial_read_until(fd,buf,'\n',buf_max,timeout);
+	//printf("%s",buf);
+	if(strcmp("ok",buf) == 0)
+		printf("%s axis set to %s\n",plane,coordinate);
+	else
+		printf("failed to set axis\n");
+	return 0;
+}
+// list all the printable items on the SD card inserted on the back of the LCD
+int listSD(int fd, char* buf) {
+	char* cmd = "M20\n";
+	int n = serial_write(fd,cmd);
+	if(n != 0)
+		return -1;
+	serial_read_until(fd,buf,'\n',buf_max,timeout);
+	printf("%s",buf);
+	return 0;
+}
+// if anything ever goes wrong, just restart it
+int restartPrinter(int fd, char* buf) {
+	char* cmd = "M999\n";
+	int n = serial_write(fd,cmd);
+	if(n != 0) 
+		return -1;
+	serial_read_until(fd,buf,'\n',buf_max,timeout);
+	printf("%s",buf);
+	return 0;
+}
 int main(int argc, char* argv[]) {
-	int buf_max = 256;
+	int opt = 0;
 	int fd = -1;
 	int baudrate = 115200; //default baudrate
-	int timeout = 5000;
-	char buf[buf_max];
 	char read[256];
 	char* readstr;
+	char buf[buf_max];
 
 	if(argc <= 1) {
-		printf("usage: %s [/dev/cu.*]\n",argv[0]);
+		usage(argv[0]);
 		return -1;
 	}
+	if(strcmp(argv[1],"-h") == 0) {
+		usage(argv[0]);
+		return 0;
+	}
+	// initialization
 	if (fd != -1) {
 		serial_close(fd);
 		printf("closed previous serial port\n");
 	}
-	fd = serial_init(argv[1],baudrate);
+	fd = serial_init(argv[2],baudrate);
 	if(fd == -1) {
 		fprintf(stderr,"%s","failed to open port\n");
 		return -1;
@@ -39,19 +103,43 @@ int main(int argc, char* argv[]) {
 	printf("opened serial port\n");
 	serial_flush(fd);
 	memset(buf,0,buf_max);
-	while(1) {
-		printf("Prusa> ");
-		fgets(read,sizeof(read),stdin);
-		readstr = read;
-		if(strcmp("/exit\n",readstr) == 0) {
-			break;
+	// now we have a connection to the serial device (probably printer in this case)
+	while ((opt = getopt(argc,argv,"alrs:")) != -1) {
+		switch(opt) {
+			case 'a':
+				if(argc <= 4) {
+					printf("not enough arguments.\n");
+					usage(argv[0]);
+					break;
+				}
+				assignCoordinate(fd,buf,argv[3],argv[4]);
+				break;
+			case 'l':
+				listSD(fd,buf);
+				break;
+			case 'r':
+				restartPrinter(fd,buf);
+				break;
+			case 's':
+				// looping shell
+				while(1) {
+					printf("Prusa> ");
+					fgets(read,sizeof(read),stdin);
+					readstr = read;
+					if(strcmp("/exit\n",readstr) == 0) {
+						break;
+					}
+					int n = serial_write(fd,readstr);
+					if (n != 0)
+						printf("error writing\n");
+					serial_read_until(fd,buf,'\n',buf_max,timeout);
+					printf("%s",buf);
+				}
+				break;
+			default:
+				usage(argv[0]);
+				break;
 		}
-		int n = serial_write(fd,readstr);
-		if (n != 0) {
-			printf("error writing\n");
-		}
-		serial_read_until(fd,buf,'\n',buf_max,timeout);
-		printf("%s",buf);
 	}
 	serial_close(fd);
 	printf("closed serial port\n");
